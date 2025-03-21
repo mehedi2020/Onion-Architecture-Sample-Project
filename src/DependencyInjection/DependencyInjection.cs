5f46135617d791b3f8fb9847cb5fccc6e74a61cc
Mehedi Hasan <mehedi4055@gmail.com>
2025-03-21 23:14:37 +1000

    Add  DependencyInjection project

---
+++ src/DependencyInjection/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿namespace SampleArchi.DependencyInjection
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplicationDependencies(this IServiceCollection services)
+        {
+            // Register Repositories
+            services.AddScoped<IProductRepository, ProductRepository>();
+
+            // Register Services
+            services.AddScoped<ProductService>();
+
+            return services;
+        }
+    }
+}
