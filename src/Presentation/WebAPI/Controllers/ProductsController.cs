7ad5d697f50739502c0320ff7499c6b2192a5865
Mehedi Hasan <mehedi4055@gmail.com>
2025-03-22 21:07:15 +1000

    Update API project

---
+++ src/Presentation/WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,56 @@
+﻿// Presentation/Controllers/ProductsController.cs
+using Microsoft.AspNetCore.Mvc;
+using SampleArchi.Core.Application.DTOS.Product;
+using SampleArchi.Core.Application.Services.ProductService;
+
+namespace Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ProductService _productService;
+
+        public ProductsController(ProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var products = await _productService.GetAllProductsAsync();
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var product = await _productService.GetProductByIdAsync(id);
+            if (product == null) return NotFound();
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(ProductDto productDto)
+        {
+            await _productService.AddProductAsync(productDto);
+            return CreatedAtAction(nameof(GetById), new { id = productDto.Id }, productDto);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, ProductDto productDto)
+        {
+            if (id != productDto.Id) return BadRequest();
+            await _productService.UpdateProductAsync(productDto);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _productService.DeleteProductAsync(id);
+            return NoContent();
+        }
+    }
+}
